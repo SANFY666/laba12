113b9d6babf9e8c29dfb0a055caeae17f5b18161
Olexandr <sanfyintrom@gmail.com>
2026-04-26 21:12:43 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace laba12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbTemplates = new System.Windows.Forms.ComboBox();
+            this.txtReceiverName = new System.Windows.Forms.TextBox();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.txtPosition = new System.Windows.Forms.TextBox();
+            this.txtSenderName = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnSearchReplace = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbTemplates
+            // 
+            this.cmbTemplates.BackColor = System.Drawing.Color.White;
+            this.cmbTemplates.FormattingEnabled = true;
+            this.cmbTemplates.Location = new System.Drawing.Point(143, 18);
+            this.cmbTemplates.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbTemplates.Name = "cmbTemplates";
+            this.cmbTemplates.Size = new System.Drawing.Size(344, 25);
+            this.cmbTemplates.TabIndex = 0;
+            // 
+            // txtReceiverName
+            // 
+            this.txtReceiverName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtReceiverName.Location = new System.Drawing.Point(144, 51);
+            this.txtReceiverName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtReceiverName.Name = "txtReceiverName";
+            this.txtReceiverName.Size = new System.Drawing.Size(343, 25);
+            this.txtReceiverName.TabIndex = 1;
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCompanyName.Location = new System.Drawing.Point(144, 84);
+            this.txtCompanyName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(343, 25);
+            this.txtCompanyName.TabIndex = 2;
+            // 
+            // txtPosition
+            // 
+            this.txtPosition.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPosition.Location = new System.Drawing.Point(144, 117);
+            this.txtPosition.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPosition.Name = "txtPosition";
+            this.txtPosition.Size = new System.Drawing.Size(343, 25);
+            this.txtPosition.TabIndex = 3;
+            // 
+            // txtSenderName
+            // 
+            this.txtSenderName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSenderName.Location = new System.Drawing.Point(144, 150);
+            this.txtSenderName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSenderName.Name = "txtSenderName";
+            this.txtSenderName.Size = new System.Drawing.Size(343, 25);
+            this.txtSenderName.TabIndex = 4;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSearch.Location = new System.Drawing.Point(143, 264);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(345, 25);
+            this.txtSearch.TabIndex = 5;
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtReplace.Location = new System.Drawing.Point(143, 300);
+            this.txtReplace.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(345, 25);
+            this.txtReplace.TabIndex = 6;
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.BackColor = System.Drawing.Color.Black;
+            this.btnPreview.FlatAppearance.BorderSize = 0;
+            this.btnPreview.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPreview.ForeColor = System.Drawing.Color.White;
+            this.btnPreview.Location = new System.Drawing.Point(280, 183);
+            this.btnPreview.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(206, 30);
+            this.btnPreview.TabIndex = 7;
+            this.btnPreview.Text = "Створити та переглянути";
+            this.btnPreview.UseVisualStyleBackColor = false;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Black;
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(280, 221);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(206, 30);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Зберегти як...";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(17, 87);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Назва компанії";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(17, 120);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Назва посади";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(17, 153);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 17);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Ім\'я відправника";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(17, 54);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 17);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Ім\'я отримувача";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 266);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Пошук";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(47, 302);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 17);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Заміна";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(16, 21);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(118, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Доступні шаблони";
+            // 
+            // btnSearchReplace
+            // 
+            this.btnSearchReplace.BackColor = System.Drawing.Color.Black;
+            this.btnSearchReplace.FlatAppearance.BorderSize = 0;
+            this.btnSearchReplace.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearchReplace.ForeColor = System.Drawing.Color.White;
+            this.btnSearchReplace.Location = new System.Drawing.Point(278, 333);
+            this.btnSearchReplace.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSearchReplace.Name = "btnSearchReplace";
+            this.btnSearchReplace.Size = new System.Drawing.Size(209, 30);
+            this.btnSearchReplace.TabIndex = 16;
+            this.btnSearchReplace.Text = "Знайти та замінити";
+            this.btnSearchReplace.UseVisualStyleBackColor = false;
+            this.btnSearchReplace.Click += new System.EventHandler(this.btnSearchReplace_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateBlue;
+            this.ClientSize = new System.Drawing.Size(512, 377);
+            this.Controls.Add(this.btnSearchReplace);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.txtReplace);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtSenderName);
+            this.Controls.Add(this.txtPosition);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.txtReceiverName);
+            this.Controls.Add(this.cmbTemplates);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Лабораторна робота №12 Зубко Олександр 201-ТК";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbTemplates;
+        private System.Windows.Forms.TextBox txtReceiverName;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.TextBox txtPosition;
+        private System.Windows.Forms.TextBox txtSenderName;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.TextBox txtReplace;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnSearchReplace;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Word = Microsoft.Office.Interop.Word; // підключення простору імен 
+
+namespace laba12
+{ 
+    public partial class Form1 : Form
+    {
+      
+        private Word.Application wordApp = null;
+        private Word.Document doc = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // красиві назви у списку шаблонів
+            cmbTemplates.Items.Add("Заява на звільнення");
+            cmbTemplates.Items.Add("Лист-подяка за рекомендацію");
+            cmbTemplates.Items.Add("Запрошення до партнерства");
+
+            if (cmbTemplates.Items.Count > 0)
+                cmbTemplates.SelectedIndex = 0;
+        }
+
+        // кнопка створення та перегляду документа
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            if (cmbTemplates.SelectedIndex == -1)
+            {
+                MessageBox.Show("вибери шаблон зі списку");
+                return;
+            }
+
+            try
+            {
+                wordApp = new Word.Application();
+
+                string selectedTemplate = cmbTemplates.SelectedItem.ToString();
+                string realFilePath = "";
+
+                // шлях до шаблонів
+                switch (selectedTemplate)
+                {
+                    case "Заява на звільнення":
+                        realFilePath = @"D:\універ\2курс\Об'єктно-орієнтоване програмування\laba 12\Templates\ResignationLetter.dotx";
+                        break;
+                    case "Лист-подяка за рекомендацію":
+                        realFilePath = @"D:\універ\2курс\Об'єктно-орієнтоване програмування\laba 12\Templates\RecommendationLetter.dotx";
+                        break;
+                    case "Запрошення до партнерства":
+                        realFilePath = @"D:\універ\2курс\Об'єктно-орієнтоване програмування\laba 12\Templates\PartnershipLetter.dotx";
+                        break;
+                }
+
+                Object templatePathObj = realFilePath;
+                Object missingObj = System.Reflection.Missing.Value;
+
+                // створення документу
+                doc = wordApp.Documents.Add(ref templatePathObj, ref missingObj, ref missingObj, ref missingObj);
+                doc.Activate();
+
+                // заповнення закладок
+                foreach (Word.FormField f in doc.FormFields)
+                {
+                    switch (f.Name)
+                    {
+                        case "ReceiverName":
+                            f.Range.Text = txtReceiverName.Text;
+                            break;
+                        case "CompanyName":
+                            f.Range.Text = txtCompanyName.Text;
+                            break;
+                        case "Position":
+                            f.Range.Text = txtPosition.Text;
+                            break;
+                        case "SenderName":
+                            f.Range.Text = txtSenderName.Text;
+                            break;
+                    }
+                }
+
+                wordApp.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка: " + ex.Message);
+                CloseWordProcesses();
+            }
+        }
+
+        //кнопка збереження документа
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (doc != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Документ Word (*.docx)|*.docx";
+                sfd.Title = "Зберегти документ";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    object filename = sfd.FileName;
+                    object missingObj = System.Reflection.Missing.Value;
+
+                    doc.SaveAs2(ref filename, ref missingObj, ref missingObj, ref missingObj, ref missingObj,
+                                ref missingObj, ref missingObj, ref missingObj, ref missingObj, ref missingObj,
+                                ref missingObj, ref missingObj, ref missingObj, ref missingObj, ref missingObj, ref missingObj);
+
+                    MessageBox.Show("Документ успішно збережено!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("спочатку створи документ за допомогою кнопки Створити та переглянути");
+            }
+        }
+
+        // кнопка пошуку та заміни тексту
+        private void btnSearchReplace_Click(object sender, EventArgs e)
+        {
+            if (doc == null)
+            {
+                MessageBox.Show("спочатку створи документ за допомогою кнопки Створити");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtSearch.Text) || string.IsNullOrWhiteSpace(txtReplace.Text))
+            {
+                MessageBox.Show("напиши текст для пошуку та текст для заміни");
+                return;
+            }
+
+            try
+            {
+                if (doc.ProtectionType != Word.WdProtectionType.wdNoProtection)
+                {
+                    object noPassword = System.Reflection.Missing.Value;
+                    doc.Unprotect(ref noPassword);
+                }
+
+                object findText = txtSearch.Text;
+                object replaceText = txtReplace.Text;
+                object replaceAll = Word.WdReplace.wdReplaceAll;
+
+                object wrap = Word.WdFindWrap.wdFindContinue;
+                object matchCase = false;
+                object missingObj = System.Reflection.Missing.Value;
+
+                Word.Find findObject = doc.Content.Find;
+                findObject.ClearFormatting();
+                findObject.Replacement.ClearFormatting();
+
+                findObject.Execute(
+                    ref findText, ref matchCase, ref missingObj, ref missingObj, ref missingObj,
+                    ref missingObj, ref missingObj, ref wrap, ref missingObj, ref replaceText,
+                    ref replaceAll, ref missingObj, ref missingObj, ref missingObj, ref missingObj);
+
+                MessageBox.Show("заміну успішно виконано");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("появилась помилка під час заміни: " + ex.Message);
+            }
+        }
+
+        // закриття процесів Word при закритті форми
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CloseWordProcesses();
+        }
+
+        // метод для закриття процесів Word
+        private void CloseWordProcesses()
+        {
+            if (doc != null)
+            {
+                object saveChanges = Word.WdSaveOptions.wdDoNotSaveChanges;
+                object missingObj = System.Reflection.Missing.Value;
+                doc.Close(ref saveChanges, ref missingObj, ref missingObj);
+                doc = null;
+            }
+            if (wordApp != null)
+            {
+                object missingObj = System.Reflection.Missing.Value;
+                wordApp.Quit(ref missingObj, ref missingObj, ref missingObj);
+                wordApp = null;
+            }
+        }
+    }
+}
+
